5f69a5e6c8a1d77bba682ae0d46be1f8c76daf86
BryanAlexisMartinez <92653045+BryanAlexisMartinez@users.noreply.github.com>
2021-12-25 22:38:56 -0600

    Add files via upload

---
+++ ProyectoVersionNodos/src/proyectoredes2/NServFlujo.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoredes2;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class NServFlujo implements Runnable {
+
+    Thread hilo;
+    int port;
+    File carpeta;
+    boolean conectado = false;
+
+    NServFlujo(int puerto, File carpeta) {
+        this.carpeta = carpeta;
+        port = puerto;
+        hilo = new Thread(this);
+        hilo.start();
+    }
+
+    public boolean isConectado() {
+        return conectado;
+    }
+
+    public void setConectado(boolean conectado) {
+        this.conectado = conectado;
+    }
+
+    @Override
+    public void run() {//envia
+        try {
+            ServerSocket s = new ServerSocket(port);
+            System.out.println("Servidor preparadado.");
+            for (;;) {
+                Socket cl = s.accept();
+                System.out.println("Conexión establecida desde" + cl.getInetAddress() + ":" + cl.getPort());
+                DataInputStream entrada = new DataInputStream(cl.getInputStream());
+                DataOutputStream salida = new DataOutputStream(cl.getOutputStream());
+                DataInputStream fis;
+                String nombre = entrada.readUTF();//leer nombre del archivo
+                if (nombre.equals("Desconexion()r86d8qRTpNVGXPC")) {
+                    setConectado(false);
+                } else {
+                    File buscado = buscar(nombre, carpeta);
+                    long tam;
+                    String archivodir;
+                    archivodir = buscado.getAbsolutePath(); //Dirección
+                    nombre = buscado.getName(); //Nombre
+                    tam = buscado.length();  //Tamaño
+                    fis = new DataInputStream(new FileInputStream(archivodir));
+                    System.out.println("" + nombre);
+                    System.out.println("Enviando '" + nombre + "', de tamaño: " + tam + "Bytes");
+                    salida.writeLong(tam);
+                    salida.flush();
+                    byte[] b = new byte[1024];
+                    long enviados = 0;
+                    int n;
+                    while (enviados < tam) {
+                        n = fis.read(b);
+                        salida.write(b, 0, n);//enviamos bytes tamaño del buffer
+                        salida.flush();
+                        enviados = enviados + n;
+                        System.out.print("Enviados: " + enviados + " de " + tam + "\r");
+                    }
+                    if (entrada.readBoolean()) {//todo bien
+                        System.out.println(nombre + " enviado exitosamente\n");
+                    } else {
+                        System.out.println("Hubo un error al enviar " + nombre + "\n");
+                    }
+                    fis.close();//cerrar flujo para archivo individual
+                    entrada.close();//cerrar flujos
+                    salida.close();
+                    cl.close();
+                    System.out.println("Conexión con el cliente terminada!");
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Nodo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    File buscar(String archivoABuscar, File directorio) {
+        System.out.println("" + archivoABuscar);
+        System.out.println(directorio.getAbsolutePath() + "   " + directorio);
+        File[] archivos = directorio.listFiles();
+        for (File archivo : archivos) {
+            if (archivo.getName().equals(archivoABuscar)) {
+                return archivo;
+            }
+            if (archivo.isDirectory()) {
+                File resultadoRecursion = buscar(archivoABuscar, archivo);
+                if (resultadoRecursion != null) {
+                    return resultadoRecursion;
+                }
+            }
+        }
+        return null;
+    }
+}
---
+++ ProyectoVersionNodos/src/proyectoredes2/SNClientMultidif.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoredes2;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
+import java.net.SocketAddress;
+import java.net.StandardProtocolFamily;
+import java.net.StandardSocketOptions;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class SNClientMultidif implements Runnable {
+
+    ArrayList<Cronometro> SuperNodos = new ArrayList<>();
+    Thread hilo;
+    int PUERTO = 2828;
+    int PUERTOrmi;
+
+    SNClientMultidif(int puerto) {
+        PUERTOrmi = puerto;
+        hilo = new Thread(this);
+        hilo.start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            DatagramChannel canal = DatagramChannel.open(StandardProtocolFamily.INET);
+            canal.configureBlocking(false);
+            NetworkInterface ni = NetworkInterface.getByName("eth3");
+            canal.setOption(StandardSocketOptions.SO_REUSEADDR, true);
+            canal.setOption(StandardSocketOptions.IP_MULTICAST_IF, ni);
+            InetSocketAddress dirr = new InetSocketAddress(PUERTO);
+            InetAddress Group = InetAddress.getByName("228.1.1.10");
+            canal.join(Group, ni);
+
+            canal.socket().bind(dirr);
+            Selector selector = Selector.open();
+            canal.register(selector, SelectionKey.OP_READ);
+            ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
+            while (true) {
+                selector.select(5000);
+                Set sk = selector.selectedKeys();
+                Iterator it = sk.iterator();
+                while (it.hasNext()) {
+                    SelectionKey key = (SelectionKey) it.next();
+                    it.remove();
+                    if (key.isReadable()) {
+                        boolean NoEsta = true;
+                        buffer.clear();
+                        String ClienteS = receiveMessage(canal);
+                        System.out.println("" + ClienteS);
+                        ClienteS = ClienteS.replace("/", "");
+                        String[] Datos = ClienteS.split(":");
+                        if (Datos.length > 1) {
+                            for (int i = 0; i < SuperNodos.size(); i++) {
+                                if (SuperNodos.get(i).contenido.equals(Datos[1])) {
+                                    NoEsta = false;
+                                    System.out.println("superNodo " + Datos[1] + " Vivo");
+                                    SuperNodos.get(i).Restablecer();
+                                }
+                            }
+                            if (NoEsta) {
+                                System.out.println("superNodo " + Datos[1] + " registrado");
+                                SuperNodos.add(new Cronometro(Datos[1], 30));
+                            }
+                        }
+                    }
+                }
+            }//while
+        } catch (IOException e) {
+            System.err.println(e);
+        }//catch
+    }
+
+    String tabla() {
+        String result = "<tr>\n"
+                + "    <td>Puerto</td><td>restante</td>"
+                + "  </tr>";
+        for (int i = 0; i < SuperNodos.size(); i++) {
+            if (SuperNodos.get(i) != null && SuperNodos.get(i).Segundos > 0) {
+                result = result + "<tr><td>" + SuperNodos.get(i).contenido + "</td>" + "<td>" + SuperNodos.get(i).Segundos + "</td></tr>";
+            }
+        }
+        return "<table>" + result + "</table>";
+    }
+
+    public static int aleatorio(int minimo, int maximo) {
+        // nextInt regresa en rango pero con límite superior exclusivo, por eso sumamos 1
+        return ThreadLocalRandom.current().nextInt(minimo, maximo + 1);
+    }
+
+    public static String receiveMessage(DatagramChannel server) throws IOException {
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        SocketAddress remoteAdd = server.receive(buffer);
+        String message = extractMessage(buffer);
+        return message;
+    }
+
+    private static String extractMessage(ByteBuffer buffer) {
+        buffer.flip();
+        byte[] bytes = new byte[buffer.remaining()];
+        buffer.get(bytes);
+        String msg = new String(bytes);
+        return msg;
+    }
+
+    String nodosVivos() {
+        String result = "";
+        for (int i = 0; i < SuperNodos.size(); i++) {
+            if (SuperNodos.get(i) != null && SuperNodos.get(i).Segundos > 0 && !SuperNodos.get(i).contenido.equals(PUERTOrmi + "")) {
+                result = result + SuperNodos.get(i).contenido + "/";
+            }
+        }
+        return result;
+    }
+}
